ce8a8662d495d861979177c52c62af83a4cf1279
AzizurRahman14 <163502416+AzizurRahman14@users.noreply.github.com>
2024-08-11 23:36:24 +0600

    add static file

--- foodOnline_main/urls.py
+++ foodOnline_main/urls.py
@@ -14,6 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 from django.contrib import admin
 from django.urls import path
 
@@ -23,3 +28,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',Views.home, name='home')
 ]
+if settings.DEBUG:  # Serve media files through Django during development
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
