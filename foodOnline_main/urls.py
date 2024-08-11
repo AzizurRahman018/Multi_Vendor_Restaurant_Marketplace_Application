558bb8d35f81128c4fb90cdaf729bdcd3a0ca305
AzizurRahman14 <163502416+AzizurRahman14@users.noreply.github.com>
2024-08-11 22:29:00 +0600

    templates

--- foodOnline_main/urls.py
+++ foodOnline_main/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from . import Views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('')
+    path('',Views.home, name='home')
 ]
