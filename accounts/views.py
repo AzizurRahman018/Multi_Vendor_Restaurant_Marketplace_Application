2c04b906d6adc9b739fb2213b0549975dfa0f39c
AzizurRahman14 <106616550+AzizurRahman018@users.noreply.github.com>
2024-08-14 23:39:10 +0600

    registeruser page created using form

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,19 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .forms import UserForm
 # Create your views here.
 def registerUser(request):
-    form = UserForm()
+    if request.method =="POST":
+        print(request.POST)
+        form= UserForm(request.POST)
+        if form.is_valid():
+           form.save()
+           return redirect('registerUser')
+    else:
+
+     form = UserForm()
     context={
         'form':form,
-
-
 
     }
 
