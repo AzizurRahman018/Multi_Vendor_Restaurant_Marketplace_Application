775b8189ed0261a79d2dd7f77f1b285b8e4ba1ac
AzizurRahman14 <106616550+AzizurRahman018@users.noreply.github.com>
2024-08-15 23:37:56 +0600

    password Hashing

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
+from accounts.models import User
 from .forms import UserForm
 # Create your views here.
 def registerUser(request):
@@ -7,7 +8,14 @@ def registerUser(request):
         print(request.POST)
         form= UserForm(request.POST)
         if form.is_valid():
-           form.save()
+           password = form.cleaned_data['password']
+           
+           user = form.save(commit=False)
+           
+           user.set_password(password)
+           
+           user.role=User.CUSTOMER
+           user.save()
            return redirect('registerUser')
     else:
 
