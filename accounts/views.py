af5c48a0858eff50242159d0b7d2ffaceb10896e
AzizurRahman14 <106616550+AzizurRahman018@users.noreply.github.com>
2024-08-19 00:35:49 +0600

    vendor

--- accounts/views.py
+++ accounts/views.py
@@ -54,9 +54,36 @@ def registerUser(request):
 
 
 def registerVendor (request):
-   
-   form = UserForm()
-   v_form = VendorForm()
+   if request.method =="POST":
+       form = UserForm(request.POST)
+       v_form = VendorForm(request.POST ,request.FILES)
+       if form.is_valid() and v_form.is_valid():
+           first_name = form.cleaned_data['first_name']
+           last_name =form.cleaned_data['last_name']
+           username = form.cleaned_data['username']
+           email = form.cleaned_data['email']
+           password = form.cleaned_data['password']
+           user = User.objects.create_user(first_name=first_name,last_name=last_name,username=username,password=password,email=email)
+           user.role= User.VENDOR
+           user.save()
+           vendor = v_form.save(commit=False)
+           vendor.user = user
+           
+           
+           
+           messages.success(request,"your account has been registered sucessfully")
+
+    
+           return redirect('registervendor')
+       
+       else:
+           print("Invalid form")
+           print(form.errors)
+   else :
+      
+
+        form = UserForm()
+        v_form = VendorForm()
    context = {
       'form':form,
       'v_form':v_form
